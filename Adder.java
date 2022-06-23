d1aef57ddb70b83fd7e99cd8113d0f01f7e0f2a4
sherinemart <91425378+sherinemart@users.noreply.github.com>
2022-06-23 13:19:36 +0530

    Update Adder.java

--- Adder.java
+++ Adder.java
@@ -23,6 +23,6 @@ public static void main(String args[])
  System.out.println("Sum of 2 integer: "+Adder.add(5,6));
  System.out.println("Sum of 2 float: "+Adder.add(2.5f,6.9f));
  System.out.println("Sum of 3 integer: "+Adder.add(9,1,4));
- System.out.println("Sum of 2 double: "+Adder.add(56.345,6.3455));
+ System.out.println("Sum of 2 double: "+Adder.add(56.345,6.37564));
 }
 }
